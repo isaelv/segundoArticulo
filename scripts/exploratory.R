ac38c13c02352874db411a3efe00acaed626b942
Isael Victoria Salazar <isaeldev@gmail.com>
2016-12-05 12:23:55 -0500

    Exploración de datos
    Gráficas de las variables de respuesta en función de la condición y
    semestre de muestreo.

--- scripts/exploratory.R
+++ scripts/exploratory.R
@@ -8,4 +8,11 @@ setwd("~/Dropbox/IVSDoctorado/segundoArticulo")
 
 groups.prop <- read.table("datos/txt/groupsProportions.txt", header = T, dec = ".")
 head(groups.prop)
-xyplot(lateSpsProp ~ factor(semester), groups.prop)
+pairs(groups.prop)
+
+par(mfrow = c(2,2))
+coplot(lateSps ~ stblAlgae | factor(semester) + condition, data = groups.prop)
+coplot(recLateSps ~ stblAlgae |factor(semester) + condition, data = groups.prop)
+coplot(earlierSps ~ factor(semester) | condition, data = groups.prop)
+coplot(recEarlierSps ~ factor(semester) | condition, data = groups.prop)
+
---
+++ training/mcmcglmm.R
@@ -0,0 +1,3 @@
+install.packages("MCMCglmm")
+library(MCMCglmm)
+data("BTdata")
