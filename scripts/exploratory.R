3554065692763b491d09903f8be9f6a65ca20456
Isael Victoria Salazar <isaeldev@gmail.com>
2016-12-06 19:56:21 -0500

    Gráfica adultos vs reclutas
    Triángulos abajo especies tardías y triángulos arriba especies
    tempranas. Falta poner leyenda.

--- scripts/exploratory.R
+++ scripts/exploratory.R
@@ -10,9 +10,22 @@ groups.prop <- read.table("datos/txt/groupsProportions.txt", header = T, dec = "
 head(groups.prop)
 pairs(groups.prop)
 
-par(mfrow = c(2,2))
-coplot(lateSps ~ stblAlgae | factor(semester) + condition, data = groups.prop)
-coplot(recLateSps ~ stblAlgae |factor(semester) + condition, data = groups.prop)
-coplot(earlierSps ~ factor(semester) | condition, data = groups.prop)
-coplot(recEarlierSps ~ factor(semester) | condition, data = groups.prop)
+####Beta regression
+##Transformation function since beta has open interval (0,1). From betareg vignette
+
+y.transf.betareg <- function(y){
+  n.obs <- sum(!is.na(y))
+  (y*(n.obs -1) + 0.5)/n.obs
+}
+
+late.b.reg <- betareg(y.transf.betareg(recLateSps) ~ lateSps + condition, data = groups.prop)
+earlier.b.reg <- betareg(y.transf.betareg(recEarlierSps) ~ earlierSps + condition, data = groups.prop)
+summary(late.b.reg)
+summary(earlier.b.reg)
+
+##Plotting
+
+plot(recLateSps ~ lateSps, data = groups.prop, col = groups.prop$condition, pch = 3)
+points(recEarlierSps ~ earlierSps, data = groups.prop, pch = 2, col = groups.prop$condition)
+
 
